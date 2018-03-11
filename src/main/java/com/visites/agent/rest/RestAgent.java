ee3bd9ac8f440eb2c9b60944b85d82d379a958ea
Thibaud REY <contact@thibaudrey.pro>
2018-03-11 18:36:36 +0100

    Fixed PathParam id on Update

--- src/main/java/com/visites/agent/rest/RestAgent.java
+++ src/main/java/com/visites/agent/rest/RestAgent.java
@@ -45,7 +45,7 @@ public class RestAgent {
     @PUT
     @Path("/{id}")
     public Response Update(
-            @QueryParam("id") int id,
+            @PathParam("id") int id,
             String body
     ) {
 
